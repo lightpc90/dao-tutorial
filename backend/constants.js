7aac953527051e6059668d3f47a0f718f4056da8
lightpc90 <afoageektech@gmail.com>
2023-02-08 16:23:58 +0100

    Contracts deployed commit

---
+++ backend/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x1Cd55a21dFBF013B41e601afbD4812e0555BB7E8";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
